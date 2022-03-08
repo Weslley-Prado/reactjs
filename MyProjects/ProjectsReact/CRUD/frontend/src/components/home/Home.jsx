bb4e69b2570e7ad362aa6b87a4cbd27251cf2a7a
Weslley Prado <weslley.prado73@gmail.com>
2022-03-08 15:40:28 -0300

    Nav was created

---
+++ MyProjects/ProjectsReact/CRUD/frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../templates/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio"
+        subtitle="Projeto React">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de exemplo de cadastro</p>
+
+    </Main>
---
+++ MyProjects/ProjectsReact/CRUD/frontend/src/components/templates/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+
+    <footer className="footer">
+        Desenvolvido com <i className="fa fa-heart text danger"></i> por 
+        <strong> WHT <span className="text-danger"> West High Technology</span></strong>
+    </footer>
